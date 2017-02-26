35b86904ac3b60a41ae01fedd10257cc4f6d1888
Mary Wang <mary.mqwang@gmail.com>
2017-02-25 19:27:08 -0500

    added map page

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,6 +4,7 @@
  * Licensed under the MIT license
  */
 
+
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
@@ -2375,3 +2376,9 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+// MAP JS code
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
